a79d15ed96662f4ba54f37f40568e793153d53f9
Amir Jafarian <amirali.jfr61@gmail.com>
2016-04-18 14:23:58 -0400

    Using mongoDB now

--- server/boot/boot.js
+++ server/boot/boot.js
@@ -1,42 +1,47 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 module.exports = function (server, done) {
   var customers = server.models.customer;
-  server.dataSources.mysqlDs.automigrate('customer', function (err) {
+  server.dataSources.mongodbDs.automigrate('customer', function (err) {
     if (err) {
       console.error("boot, ERR:", err);
       done(err);
     } else {
 
+      // Queries the customers model
+      function showData() {
+        customers.find(function(err, data){
+          console.log('Final Data:', data[0]);
+          done(err);
+        });
+      }
+
 
       // replaceOrCreate for existing id
-//      customers.create({
-//        firstName: 'Amirali',
-//        lastName: 'Jafarian',
-//        age: 30},
-//      function(err, inst) {
-//        customers.replaceOrCreate({
-//          id: inst.id,
-//          firstName: 'Amir1'
-//        }, function(err, inst) {
-//          console.log('inst:', inst); // { firstName: 'Amir1', id: 1 }
-//          done(err);
-//        });
-//      });
+      customers.create({
+        firstName: 'Amirali',
+        lastName: 'Jafarian',
+        age: 30},
+      function(err, inst) {
+        if (err) return done(err);
+        console.log('created inst:', inst);
+        customers.replaceOrCreate({
+          id: inst.id,
+          firstName: 'Amir1'
+        }, function(err, inst) {
+          if (err) return done(err);
+          showData();
+        });
+      });
 
 
       // replaceOrCreate for non existing id
 //      customers.replaceOrCreate({
 //        id: 99,
-//        firstName: 'Amir2'
+//        firstName: 'Amir2',
+//        lastName: 'Jafarian',
+//        age: 30
 //      }, function(err, inst) {
-//        console.log('inst:', inst); // { firstName: 'Amir2', id: 99 }
-//        done(err);
+//          if (err) return done(err);
+//          showData();
 //      });
 
 
@@ -45,7 +50,12 @@ module.exports = function (server, done) {
 //        firstName: 'Amirali',
 //        lastName: 'Jafarian',
 //        age: 30}, function (err, inst) {
-//        customers.replaceById(inst.id, {firstName: 'Amir3'}, done); // { firstName: 'Amir3', id: 1 }
+//        if (err) return done(err);
+//        console.log('created inst:', inst);
+//        customers.replaceById(inst.id, {firstName: 'Amir3'}, function(err, inst) {
+//          if (err) return done(err);
+//          showData();
+//        });
 //      });
       
 
@@ -55,7 +65,11 @@ module.exports = function (server, done) {
 //        lastName: 'Jafarian',
 //        age: 30},
 //      function (err, inst) {
-//        inst.replaceAttributes({firstName: 'Amir4'}, done); // { firstName: 'Amir4', id: 1 }
+//        console.log('created inst:', inst);
+//        inst.replaceAttributes({firstName: 'Amir4'}, function(err, data) {
+//          if (err) return done(err);
+//          showData();
+//        }); 
 //      });
 
       
@@ -64,59 +78,58 @@ module.exports = function (server, done) {
           firstName: 'Amirali',
           lastName: 'Jafarian',
           age: 30
-        };      
+        };
 //        customers.create(data)
 //          .then(function(created) {
 //            created = created.toObject();
-//            delete created.lastName;
-//            delete created.age;
-//            created.firstName = 'Amir5';
-//            return customers.replaceOrCreate(created)
+//            console.log('created:', created);
+//            return customers.replaceOrCreate({
+//              id: created.id,
+//              firstName: 'Amir5'
+//            })
 //            .then(function(inst) {
-//              console.log('inst:', inst); // { firstName: 'Amir5', id: 1 }
-//              done();
+//              showData();
 //            });
 //          })
 //        .catch(done);
 
 
-//        // Promise version for replaceAttributes
+        // Promise version for replaceAttributes: (a prototype method)
 //        customers.create(data)
-//          .then(function(created) {
-//            return created.replaceAttributes({firstName: 'Amir6'})
-//            .then(function(inst) { // { firstName: 'Amir6', id: 1 }
-//              done();
+//          .then(function(inst) {
+//            console.log('created:', inst);
+//            return inst.replaceAttributes({firstName: 'Amir6'})
+//            .then(function(created) { 
+//              showData();
 //            });
 //          })
 //        .catch(done);
 
 
-        // Promise version for replaceById
+        // Promise version for replaceById: (a static method)
 //        customers.create(data)
 //          .then(function(created) {
+//            console.log('created:', created);
 //            return customers.replaceById(created.id, {firstName: 'Amir7'})
-//            .then(function(inst) { // { firstName: 'Amir7', id: 1 }
-//              done();
+//            .then(function(inst) {
+//              showData();
 //            });
 //          })
 //        .catch(done);
 
 
       // An example of Operation hooks
-      customers.observe('before save', function (ctx, next) {
-        ctx.instance.firstName = 'Amir8';
-        next();
-      });
-
-      customers.replaceOrCreate({
-        firstName: 'Amirali'
-      }, function (err, instance) {
-        if (err) {
-          return done(err);
-        }
-        console.log('inst:', instance);
-        done();
-      });
+//      customers.observe('before save', function (ctx, next) {
+//          ctx.instance.firstName = 'Amir8';
+//        next();
+//      });
+//
+//      customers.replaceOrCreate({
+//        firstName: 'Amirali'
+//      }, function (err, instance) {
+//        if (err) return done(err);
+//        showData();
+//      });
 
 
     }
